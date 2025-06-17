abaaa4a3d9b63177bf0417dee8e2e8a300b52d11
Yersultan Kylyshev <ersultan272001@gmail.com>
2025-06-17 11:49:26 +0200

    done

--- ReflectionExceptions/ExceptionsTutorial/Program.cs
+++ ReflectionExceptions/ExceptionsTutorial/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Welcome to the numbers adder program. This program will add two integers for you");
+Console.WriteLine("Welcome to the numbers adder program. This program will add two integers for you");
 
 Console.WriteLine("Enter the first number");
 var first = Console.ReadLine();
@@ -6,9 +6,20 @@ var first = Console.ReadLine();
 Console.WriteLine("Enter the second number");
 var second = Console.ReadLine();
 
-var f = int.Parse(first);
-var s = int.Parse(second);
+try
+{
+    var f = int.Parse(first);
+    var s = int.Parse(second);
 
-var result = f + s;
+    var result = f + s;
 
-Console.WriteLine($"The result is {result}");
+    Console.WriteLine($"The result is {result}");
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Improper format");
+}
+catch (OverflowException e)
+{
+    Console.WriteLine("Value out of range");
+}
--- ReflectionExceptions/ReflectionTutorial/Program.cs
+++ ReflectionExceptions/ReflectionTutorial/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+using ReflectionTutorial;
+using System.Reflection;
+
+var data = File.ReadAllText("car.txt").Split(";");
+var brand = data[0];
+var model = data[1];
+var tankCapacity = int.Parse(data[2]);
+var fuelConsumption = double.Parse(data[3]);
+var fuelLevel = int.Parse(data[4]);
+var odometer = int.Parse(data[5]);
+
+//To do on your own 4
+var carType = typeof(Car);
+var carobj = Activator.CreateInstance(carType, brand, model, tankCapacity, fuelConsumption);
+
+var fuelLevelField = carType.GetField("_fuelLevel", BindingFlags.NonPublic | BindingFlags.Instance);
+var odometerField = carType.GetField("_odometer", BindingFlags.NonPublic | BindingFlags.Instance);
+
+fuelLevelField.SetValue(carobj, fuelLevel);
+odometerField.SetValue(carobj, odometer);
+
+Car car = (Car)carobj;
+
+Console.WriteLine($"{car.Brand} {car.Model} drove {car.Odometer} kilometers and remaining fuel is {car.FuelLevel}");
